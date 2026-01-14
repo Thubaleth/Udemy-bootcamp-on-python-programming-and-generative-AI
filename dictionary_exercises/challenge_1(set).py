a19ba2c2ee8bd53f5a70d80695dd01f422981d79
Thubalethu Njapha <thubzthubz@gmail.com>
2026-01-14 22:38:24 +0200

    dict practice

---
+++ dictionary_exercises/challenge_1(set).py
@@ -0,0 +1,6 @@
+#Write a Python script that removes all duplicate elements from a list in a single line using sets.
+
+L1 = [1,2,1,1,2,3,1,1]
+
+unique_list = set(L1)
+print(unique_list)
---
+++ dictionary_exercises/challenge_2().py
@@ -0,0 +1,20 @@
+"""
+Challenge #2
+
+Write a Python script that counts and displays the occurrences of each character in a list.
+Example:
+Input: list('mamma mia mm')
+Output:
+
+m ---> 6
+
+a ---> 3
+
+---> 2
+
+i ---> 1
+"""
+
+l1  = list('mamma mia mm')
+
+

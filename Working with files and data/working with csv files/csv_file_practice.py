f46fc491477715d3a00c332c492f5db806149e1b
Thubalethu Njapha <thubzthubz@gmail.com>
2026-05-19 16:27:11 +0200

    Max generated token on CSV file

--- Working with files and data/working with csv files/csv_file_practice.py
+++ Working with files and data/working with csv files/csv_file_practice.py
@@ -1,16 +1,17 @@
 import csv
 
-with open('working with csv files\model_logs.csv','r') as csvfile:
+with open('model_logs.csv','r') as csvfile:
     reader = csv.reader(csvfile)#reads each row as a list
     next(reader)
     token_data = {}
 
-for row in reader:
-    key = row[0]
-    value = int(row[3])
+    for row in reader:
+      key = row[0]
+      value = int(row[3])
 
-    token_data[key] = value
+      token_data[key] = value #To store each day as a key and the numbers as a value
     
+    peak_day = max(token_data, key = token_data.get) 
     
-print(token_data) #To store each day as a key and the numbers as a value
+    print(f'Model usage: {peak_day}, Token Generated: {token_data[peak_day]}' ) #Store the max generated token
 
